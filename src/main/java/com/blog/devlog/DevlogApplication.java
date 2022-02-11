d94ac53e019e570def36a9542a5f6b11d2405560
meeyoungchoi <goodlife1359@gmail.com>
2022-02-11 22:41:53 +0900

    02월 11일 프로젝트 환경설정

---
+++ src/main/java/com/blog/devlog/DevlogApplication.java
@@ -0,0 +1,13 @@
+package com.blog.devlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevlogApplication.class, args);
+	}
+
+}
